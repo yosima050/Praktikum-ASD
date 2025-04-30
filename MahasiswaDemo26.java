a4efdd1744a0ecb414429a33716a4f119555fd2b
yosima050 <byosep050@gmail.com>
2025-04-30 14:21:46 +0700

    Percobaan 1

---
+++ MahasiswaDemo26.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class MahasiswaDemo26 {
+    public static void main(String[] args) {
+        StackTugasMahasiswa26 stack = new StackTugasMahasiswa26(5);
+
+        Scanner scan = new Scanner(System.in);
+        int pilih;
+
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Mengumpulkan Tugas");
+            System.out.println("2. Menilai Tugas");
+            System.out.println("3. Melihat Tugas Teratas");
+            System.out.println("4. Melihat Daftar Tugas");
+            System.out.println("5. Melihat Tugas Pertama Dikirim");
+            System.out.println("6. Melihat Jumlah Tugas yang Terkumpul");
+            System.out.print("Pilih: ");
+            pilih = scan.nextInt();
+            scan.nextLine();
+
+            switch (pilih) {
+                case 1:
+                    System.out.print("Nama: ");
+                    String nama = scan.nextLine();
+                    System.out.print("NIM: ");
+                    String nim = scan.nextLine();
+                    System.out.print("Kelas: ");
+                    String kelas = scan.nextLine();
+                    Mahasiswa26 mhs = new Mahasiswa26(nim, nama, kelas);
+                    stack.push(mhs);
+                    System.out.printf("Tugas %s berhasil dikumpulkan\n", mhs.nama);
+                    break;
+
+                case 2:
+                    Mahasiswa26 dinilai = stack.pop();
+                    if (dinilai != null) {
+                        System.out.println("Menilai tugas dari " + dinilai.nama);
+                        System.out.print("Masukkan nilai (0-100): ");
+                        int nilai = scan.nextInt();
+                        scan.nextLine();
+                        dinilai.tugasDinilai(nilai);
+                        System.out.printf("Nilai Tugas %s adalah %d\n", dinilai.nama, nilai);
+                    }
+                    break;
+
+                case 3:
+                    Mahasiswa26 lihat = stack.peek();
+                    if (lihat != null) {
+                        System.out.println("Tugas terakhir dikumpulkan oleh " + lihat.nama);
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Daftar semua tugas");
+                    System.out.println("Nama\tNIM\tKelas");
+                    stack.print();
+                    break;
+
+                case 5:
+                    Mahasiswa26 bawah = stack.peekBottom();
+                    if (bawah != null) {
+                        System.out.println("Tugas pertama dikumpulkan oleh " + bawah.nama);
+                    }
+                    break;
+                
+                case 6:
+                    System.out.println("Jumlah tugas yang sudah dikumpulkan: " + stack.jumlahTugas());
+                    break;
+                
+                default:
+                    System.out.println("Pilihan tidak valid.");
+            }
+        } while (pilih >= 1 && pilih <= 6);
+    }
+}
---
+++ StackTugasMahasiswa26.java
@@ -0,0 +1,77 @@
+public class StackTugasMahasiswa26 {
+    Mahasiswa26[] stack;
+    int top;
+    int size;
+
+    public StackTugasMahasiswa26(int size) {
+        this.size = size;
+        stack = new Mahasiswa26[size];
+        top = -1;
+    }
+
+    public int jumlahTugas() {
+        return top + 1;
+    }
+    
+    public boolean isFull() {
+        if (top == size -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(Mahasiswa26 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
+
+    public Mahasiswa26 pop() {
+        if (!isEmpty()) {
+            Mahasiswa26 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+
+    public Mahasiswa26 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return null;
+        }
+    }
+
+    public Mahasiswa26 peekBottom() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return null;
+        }
+    }
+    
+
+    public void print() {
+        for (int i = top; i >= 0; i--) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println();
+    }
+}
